404be0a6a2e0d215b50888d63213c2cf1fada20e
murthehelp24 <attachaijumpahom@gmail.com>
2026-05-23 23:48:20 +0700

    feat: initialize Prisma schema with User, Card, Order, and Wishlist models and configure Prisma client instance

--- API/src/lib/prisma.js
+++ API/src/lib/prisma.js
@@ -1,6 +1,9 @@
+import "dotenv/config";
 import { PrismaClient } from "../generated/prisma/client.js";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasourceUrl: process.env.DATABASE_URL,
+});
 
 export { prisma };
  
