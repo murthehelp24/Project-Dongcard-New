a50e95752dcf01ac94576b30c801da432a3dce0a
murthehelp24 <attachaijumpahom@gmail.com>
2026-05-23 17:03:44 +0700

    depoy ver 0.5

--- Frontend/src/api/mainApi.js
+++ Frontend/src/api/mainApi.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 
 export const mainApi = axios.create({
-  baseURL: 'http://localhost:8888/api'
+  baseURL: 'project-dongcard-new-85wo.vercel.app'
 })
 
 mainApi.interceptors.request.use(config => {
