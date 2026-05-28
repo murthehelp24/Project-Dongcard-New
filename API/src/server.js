e18ed486784b2db8087d597d4d3603495e1721ed
murthehelp24 <attachaijumpahom@gmail.com>
2026-05-28 11:53:07 +0700

    depoy ver 1.2

--- API/src/server.js
+++ API/src/server.js
@@ -3,10 +3,8 @@ import app from './app.js'
 
 const PORT = process.env.PORT || 8000
 
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, ()=>{
-    console.log(`Server Start : http://localhost:${PORT}`)
-  })
-}
+app.listen(PORT, ()=>{
+  console.log(`Server Start : http://localhost:${PORT}`)
+})
 
 export default app
