9e3247b6006709b26f602fc129d7f6fe605e8d9d
murthehelp24 <attachaijumpahom@gmail.com>
2026-05-28 11:49:00 +0700

    depoy ver 1.0

--- API/src/lib/prisma.js
+++ API/src/lib/prisma.js
@@ -1,7 +1,11 @@
-import { PrismaClient } from "../generated/prisma/index.js";
+import "dotenv/config";
+import { PrismaPg } from '@prisma/adapter-pg'
+import pg from 'pg'
+import prismaPkg from '../generated/prisma/index.js';
+const { PrismaClient } = prismaPkg;
 
-const prisma = new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL,
-});
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL })
+const adapter = new PrismaPg(pool)
+const prisma = new PrismaClient({ adapter })
 
 export { prisma };
